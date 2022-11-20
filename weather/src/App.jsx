2f40b5c0e54e4a808100149042a902269ab906e3
byrjvkhln <battamir08@gmail.com>
2022-11-20 21:33:47 +0800

    weather done

--- weather/src/App.jsx
+++ weather/src/App.jsx
@@ -14,17 +14,50 @@ const App = () => {
     };
     dataAvagc();
   }, []);
-  // const faranheit = (temp) => {
-  //   return (5 / 9) * (temp - 32);
-  // };
-  useEffect(() => {
-    console.log(info.timezone);
-  });
+  const faranheit = (temp) => {
+    return (5 / 9) * (temp - 32);
+  };
+  const timer = (unix_timestamp) => {
+    unix_timestamp = unix_timestamp + 1;
+    let date = new Date(unix_timestamp * 1000);
+    let hours = date.getHours();
+    let minutes = "0" + date.getMinutes();
+    return hours + ":" + minutes.substr(-2);
+  };
   return (
     <div className="container">
-      <div>{info.timezone}</div>
+      {/* <div>{info.timezone}</div>
       <div>{info?.currently?.temperature}+°C</div>
-      <div>{info?.currently?.summary}</div>
+      <div>{info?.currently?.summary}</div> */}
+      <div
+        style={{
+          width: "30vw",
+
+          borderRadius: "15px",
+          overflow: "hidden",
+          boxShadow: "rgba(0, 0, 0, 0.35) 0px 5px 15px",
+        }}
+      >
+        <div className="main ">
+          <div
+            className="font"
+            style={{
+              backgroundColor: "rgb(34, 34, 34)",
+              width: "100%",
+              color: "white",
+              textAlign: "center",
+            }}
+          >
+            {info.timezone}
+          </div>
+          <div className="font temp">
+            <div>{Math.trunc(faranheit(info?.currently?.temperature))}°C</div>
+            <div>{info?.currently?.summary}</div>
+          </div>
+
+          <div className="font"> {timer(info?.currently?.time)}</div>
+        </div>
+      </div>
     </div>
   );
 };
